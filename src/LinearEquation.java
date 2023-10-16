0ff90bae11ccbae0e437b08fad2b3999174a7a8f
BT_2N2_34 <sample@bths.edu>
2023-10-16 13:59:44 -0400

    First

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int coord1y;
+    private int coord1x;
+    private int coord2y;
+    private int coord2x;
+
+    public LinearEquation (int coord1x, int coord1y, int coord2x, int coord2y) {
+        this.coord1x = coord1x;
+
+    }
+
+    public double distance ()
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+public class LinearEquationRunner {
 }
